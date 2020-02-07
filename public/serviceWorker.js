8194df77535ccababd5b788c8dede4efe92f6080
Craig Gant <craigkgant@gmail.com>
2020-02-07 09:44:30 -0500

    corrected script for service worker in index.html

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,8 +1,9 @@
 // see code from week 9
 const FILES_TO_CACHE = [
-  "/", 
-  "/index.html", 
-  "app.js"
+  "/",
+  "/index.html",
+  "/index.js",
+  "/favicon.ico"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -19,10 +20,11 @@ self.addEventListener("install", (evt) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   )
+  self.skipWaiting();
 });
 
 // On activation...
-self.addEventListener('activate', (evt) => {
+self.addEventListener("activate", (evt) => {
   //tells the browser that work is ongoing until he promise settles
   evt.waitUntil(
     // returns an array enumerable properties of the caches
@@ -40,11 +42,11 @@ self.addEventListener('activate', (evt) => {
       )
     })
   )
-
+  self.clients.claim();
 });
 
 // on fetch...
-self.addEventListener('fetch', (evt) => {
+self.addEventListener("fetch", (evt) => {
   // if the event includes an api call
   if (evt.request.url.includes("/api/")) {
     // logs the event
@@ -58,17 +60,18 @@ self.addEventListener('fetch', (evt) => {
           .then(response => {
             // if the response came back good, clone it and store in the cache
             if (response.status === 200) {
-              cache.put(evt.request.url, response.clone())
+              cache.put(evt.request.url, response.clone());
             }
-            return response;  
+            return response;
           })
           // catch any errors
           .catch(err => {
             // if something went wrong, try to retrieve from the cache
-            return cache.match(evt.request); 
+            return cache.match(evt.request);
           })
       })
-    )
+    );
+    return;
   }
   // on event, this prevents the browser's default behavior and allows for a promise-based response
   evt.respondWith(
