141069fcb908ceaa43e60eb706fdbb53136fc0f4
Craig Gant <craigkgant@gmail.com>
2020-02-06 19:43:41 -0500

    added content to serviceWorker.js

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-<!-- get icons and put them in an icons folder (so that the manifest will be able to use them) -->
-
-<!-- put service worker statement in index.html -->
-
 <!-- NEED TO RUN MONGOD -->
 
 <!-- we need to make sure that there's a running total at the top of the page -->
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,19 +1,84 @@
 // see code from week 9
-const FILES_TO_CACHE = {
-
-
-};
+const FILES_TO_CACHE = [
+  "/", 
+  "/index.html", 
+  "app.js"
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1"
 
+// On install, run this script
+self.addEventListener("install", (evt) => {
+  //tells the browser that work is ongoing until he promise settles
+  evt.waitUntil(
+    // opens the cache 'static-cache-v2'
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("Your files were pre-cached sucessfully.");
+      // caches documents from 'FILES TO CACHE'
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  )
+});
 
-self.addEventListener('install', function(evt)){
-  
-};
-self.addEventListener('activate', function(evt)){
+// On activation...
+self.addEventListener('activate', (evt) => {
+  //tells the browser that work is ongoing until he promise settles
+  evt.waitUntil(
+    // returns an array enumerable properties of the caches
+    caches.keys().then(keyList => {
+      // returns a single promise after all others have been fulfilled.
+      return Promise.all(
+        // maps over all keys in the keyList
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            //deletes key if above perameters are met
+            return caches.delete(key);
+          }
+        })
+      )
+    })
+  )
 
-};
-self.addEventListener('fetch', function(evt)){
+});
 
-};
+// on fetch...
+self.addEventListener('fetch', (evt) => {
+  // if the event includes an api call
+  if (evt.request.url.includes("/api/")) {
+    // logs the event
+    console.log("[Service Worker] Fetch (data)", evt.request.url);
+    //prevents the browser's default fetch handling, and allows for a promise-based response
+    evt.respondWith(
+      // opens data cache and then...
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        // returns the fetch request
+        return fetch(evt.request)
+          .then(response => {
+            // if the response came back good, clone it and store in the cache
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone())
+            }
+            return response;  
+          })
+          // catch any errors
+          .catch(err => {
+            // if something went wrong, try to retrieve from the cache
+            return cache.match(evt.request); 
+          })
+      })
+    )
+  }
+  // on event, this prevents the browser's default behavior and allows for a promise-based response
+  evt.respondWith(
+    // opens cache
+    caches.open(CACHE_NAME).then(cache => {
+      // attempts to retrieve the event request from our cache
+      return cache.match(evt.request).then(response => {
+        // either returns the response or calls a fetch
+        return response || fetch(evt.request);
+      })
+    })
+  )
+});
